a7e5fc495273c1d594c33ee848b23a1340c3183f
suyusheng123 <b3208747550@outlook.com>
2024-07-14 21:52:09 +0800

    搜索中台后端模板，暂时还没有改进

---
+++ src/main/java/com/bxw/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.bxw.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.bxw.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
